934725b51477e29d04f057cc21ee7e59e64ade44
WambuaSimon <simonwambua433@gmail.com>
2019-04-24 17:51:35 +0300

    added passport authentication

--- routes/api.php
+++ routes/api.php
@@ -11,29 +11,30 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::prefix('v1')->group(function () {
-    Route::resource('agents', 'AgentController');
-    Route::resource('documents', 'DocumentsController')->except('show');
-    Route::get('document/{DocType}', 'DocumentsController@show')->name('documents.show');
-    Route::get('groups/{id}', 'GroupController@show');
-    Route::resource('groups', 'GroupController')->except('show');
-    Route::resource('status', 'StatusController')->except('show');
-    Route::get('status/{AppStat}', 'StatusController@show')->name('status.show');
-    Route::resource('workflow','WorkflowController');
+    // Route::resource('agents', 'AgentController');
 
 });
 
-
-Route::prefix('v1')->group(function(){
+Route::prefix('v1')->group(function () {
     Route::post('login', 'UsersController@login');
 
-    Route::group(['middleware' => 'auth:api'], function(){
-    Route::post('getUser', 'UsersController@getUser');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('getUser', 'UsersController@getUser');
+
+        Route::resource('documents', 'DocumentsController')->except('show');
+        Route::get('document/{DocType}', 'DocumentsController@show')->name('documents.show');
+        Route::get('groups/{id}', 'GroupController@show');
+        Route::resource('groups', 'GroupController')->except('show');
+        Route::resource('status', 'StatusController')->except('show');
+        Route::get('status/{AppStat}', 'StatusController@show')->name('status.show');
+        Route::resource('workflow', 'WorkflowController');
+
     });
-   });
+});
